ac8d26355e0d1f2885ba89837f5c4a8a28ca12ca
911samuel <abayizeraeaz@gmail.com>
2024-02-01 17:49:27 +0200

    upddated

--- script.js
+++ script.js
@@ -10,70 +10,72 @@ function save(event) {
     localStorage.setItem("sender-message", document.getElementById('message').value);
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('load', () => {
+        document.getElementById('signupForm').addEventListener('submit', (event) => {
+            const signupEmail = document.getElementById('signupEmail').value;
+            const signupPassword = document.getElementById('signupPassword').value;
 
-document.getElementById('signupForm').addEventListener('submit', (event) => {
-    const myEmail = document.getElementById('email').value;
-    const myPassword = document.getElementById('password').value;
+            if (signupEmail === "" || signupPassword === "") {
+                alert("Please fill out all fields");
+            } else if (signupPassword.length < 8) {
+                alert("Password must be at least 8 characters long");
+            } else if (!(/[A-Z]/.test(signupPassword))) {
+                alert("Password must contain at least one uppercase letter.");
+            } else if (!/[a-z]/.test(signupPassword)) {
+                alert("Password must contain at least one lowercase letter.");
+            } else if (!isNaN(signupPassword.charAt(0))) {
+                alert("Password cannot start with a number.");
+                return; 
+            } else {
+                save(event);
+                alert("Account created successfully!");
+                return true;
+            }
 
-    if (myEmail === "" || myPassword === "") {
-        alert("Please fill out all fields");
-    } else if (myPassword.length < 8) {
-        alert("Password must be at least 8 characters long");
-    } else if (!(/[A-Z]/.test(myPassword))) {
-        alert("Password must contain at least one uppercase letter.");
-    } else if (!/[a-z]/.test(myPassword)) {
-        alert("Password must contain at least one lowercase letter.");
-    } else if (!isNaN(myPassword.charAt(0))) {
-        alert("Password cannot start with a number.");
-        return; 
-    } else {
-        save(event);
-        alert("Account created successfully!");
-        return true;
-    }
+            event.preventDefault();
+        });
 
-    event.preventDefault();
+        document.getElementById('signinForm').addEventListener('submit', function login(event) {
+            event.preventDefault();
+
+            const signinEmail = document.getElementById('signinEmail').value;
+            const signinPassword = document.getElementById('signinPassword').value;
+
+            let loginEmail = localStorage.getItem('my-email');
+            let loginPassword = localStorage.getItem('my-password');
+
+            if (signinEmail === loginEmail) {
+                if (signinPassword === loginPassword) {
+                    alert(`Welcome back, ${localStorage.getItem('my-first-name')}`);
+                } else {
+                    alert('Wrong Password');
+                }
+            } else {
+                alert('User not found! Please create an account first.');
+            }
+        });
+
+        document.getElementById('contactForm').addEventListener('submit', (event) => {
+            const form = document.getElementById('contactForm');
+
+            if (form && form.checkValidity()) {
+                save(event);
+                alert("Thank you for filling out the form!");
+            } else {
+                alert("Please fill out the form before submitting.");
+            }
+
+            event.preventDefault();
+        });
+    });
 });
-
-document.getElementById('signinForm').addEventListener('submit', function login(event) {
-    event.preventDefault();
-
-    const myEmail = document.getElementById('email').value;
-    const myPassword = document.getElementById('password').value;
-
-    let loginEmail = localStorage.getItem('my-email');
-    let loginPassword = localStorage.getItem('my-password');
-
-    if (myEmail === loginEmail) {
-        if (myPassword === loginPassword) {
-            alert(`Welcome back, ${localStorage.getItem('my-first-name')}`);
-        } else {
-            alert('Wrong Password');
-        }
-    } else {
-        alert('User not found! Please create an account first.');
-    }
-});
-
-document.getElementById('contactForm').addEventListener('submit', (event) => {
-    const form = document.getElementById('contactForm');
-
-    if (form && form.checkValidity()) {
-        save(event);
-        alert("Thank you for filling out the form!");
-    } else {
-        alert("Please fill out the form before submitting.");
-    }
-
-    event.preventDefault();
-});
-
 
 function toggleMenu() {
     const navElement = document.querySelector('.nav');
     const verticalLineElement = document.querySelector('.vertical-line');
     const contactElement = document.querySelector('.contact');
-    
+
     navElement.classList.toggle('show-menu');
     verticalLineElement.classList.toggle('hide-element');
     contactElement.classList.toggle('hide-element');
